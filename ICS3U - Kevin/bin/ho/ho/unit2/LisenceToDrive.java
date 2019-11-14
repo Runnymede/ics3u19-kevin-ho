54a6605dfd48478da5076b86277cd987e7b25222
kxvinh <55457676+kxvinh@users.noreply.github.com>
2019-11-14 13:39:27 -0500

    Add files via upload

--- ICS3U - Kevin/bin/ho/ho/unit2/LisenceToDrive.java
+++ ICS3U - Kevin/bin/ho/ho/unit2/LisenceToDrive.java
@@ -12,11 +12,14 @@ public class LisenceToDrive {
 	 * Beginning of the program.
 	 * @param args
 	 */
-	
+
 	public static void main(String[] args) {
-	
+
+		//variables
+		int probability = 0; //stores the total number of combinations that add up to 34 or greater
+
 		for (int lisence = 9999; lisence >= 0; lisence--) {
-			
+
 			int division = 1000; //same code as DigitsSum.java but with an extra 0 in division and mod to compensate for the 4th digit
 			int mod = 10000;
 			int digits = lisence%mod/division;
@@ -30,12 +33,18 @@ public class LisenceToDrive {
 				mod = mod /10;
 
 			} while (mod >= 10);
-			
-			if (total >= 34)
-				System.out.println(lisence); //prints the license number
-			
+
+			if (total >= 34) {
+
+				probability++;
+				System.out.print(lisence + " "); //prints the license number
+				
+			}
 		}
-		
+
+		System.out.println(" ");
+		System.out.println("The probability of the sum being 34 or greater is " + probability + "/10 000."); //prints the total probability
+
 	}
 
 }
