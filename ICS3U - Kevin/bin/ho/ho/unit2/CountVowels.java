6ef03a793e897711e720c81cdef02292343e257f
kxvinh <55457676+kxvinh@users.noreply.github.com>
2019-11-19 12:59:04 -0500

    Add files via upload

---
+++ ICS3U - Kevin/bin/ho/ho/unit2/CountVowels.java
@@ -0,0 +1,45 @@
+package ho.unit2;
+
+/**CountVowels.java
+ * CountVowels is an application that prompts the user for a string and then displays a count of the number of vowel in the string. 
+ * @author Kevin Ho
+ *November 12, 2019
+ */
+
+import java.util.Scanner;
+
+public class CountVowels {
+
+	/**
+	 * Beginning of the program. 
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		//variables
+		int vowelCount = 0; //stores the data for the amount of values
+		
+		//input
+		System.out.println("Enter text: ");
+		String text = sc.nextLine();
+		
+		for (int character = 0; character < text.length(); character++) {
+			
+			char vowel = text.charAt(character); //converts each of the characters into char values
+			
+			if (vowel == 65 || vowel == 69 || vowel == 73 || vowel == 79 || vowel == 85 || vowel == 89 || vowel == 97 || vowel == 101 || vowel == 105 || vowel == 111 || vowel == 117 || vowel == 121) {
+				//checks whether the character is a vowel in lower case or uppercase form
+				
+				vowelCount += 1; //increases the vowel count by one if the character is a vowel
+			}
+		}
+		
+		System.out.print("The number of vowels in " + text + " is " + vowelCount + ".");
+		
+		sc.close();
+	}
+
+}
