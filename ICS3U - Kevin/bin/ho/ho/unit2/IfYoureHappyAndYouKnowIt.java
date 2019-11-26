d30f08ecb5202454bd95dd243b3074e95b0b044a
kxvinh <55457676+kxvinh@users.noreply.github.com>
2019-11-26 15:06:04 -0500

    Add files via upload

---
+++ ICS3U - Kevin/bin/ho/ho/unit2/IfYoureHappyAndYouKnowIt.java
@@ -0,0 +1,22 @@
+package ho.unit2;
+
+/**IfYoureHappyAndYouKnowIt.java
+ * IfYoureHappyAndYouKnowIt.java is a  program that prints out the lyrics to “if you’re happy and you know it” with 10 verses.
+ * @author Kevin Ho
+ *
+ */
+
+public class IfYoureHappyAndYouKnowIt {
+
+	/**
+	 * Beginning of the program.
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+	
+		
+		
+	}
+
+}
