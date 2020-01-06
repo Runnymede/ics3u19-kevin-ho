2a71975215d859157214accf20f0ff716be66285
348735036 <348735036@C-3ZZHYR1.tdsb.on.ca>
2020-01-06 15:05:52 -0500

    Mathplus

---
+++ ICS3U - Kevin/src/ho/unit4/MathPlus.java
@@ -0,0 +1,28 @@
+package ho.unit4;
+
+/**MathPlus.java
+ * 
+ * @author Kevin Ho
+ *
+ */
+
+public class MathPlus {
+
+	public static void main(String[] args) {
+		
+	int[] mainArray = {15,2,7,4,1};
+
+	}
+
+	public static int min(int[] mainArray, int checker) {
+		
+		int arrayCounter = 0;
+		int numberCounter = 0;
+		
+		checker = mainArray[0];
+		
+		return checker;
+		
+	}
+	
+}
