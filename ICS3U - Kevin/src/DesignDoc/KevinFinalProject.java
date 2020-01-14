58b1cec11750f26682fddc18741a8b36f7e8252a
348735036 <348735036@C-3ZZHYR1.tdsb.on.ca>
2020-01-14 14:37:49 -0500

    Hi

--- ICS3U - Kevin/src/DesignDoc/KevinFinalProject.java
+++ ICS3U - Kevin/src/DesignDoc/KevinFinalProject.java
@@ -11,17 +11,51 @@ import java.util.Scanner;
 public class KevinFinalProject {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		boolean playAgain;
-		
-		
-		//input
-		System.out.println("Would you like to play against the computer or another player?");
-		String answer = sc.next();
-		
+		boolean playAgain2;
 
+		do {
+			//input
+			System.out.println("Would you like to play against the computer or another player?");
+			String answer = sc.next();
+
+			System.out.println("How many rounds do you want to go up to?");
+			int rounds = sc.nextInt();
+
+			//variables
+			boolean win = true;
+
+			if(answer.equalsIgnoreCase("computer"))
+				playerVsAI(win);
+			else if(answer.equalsIgnoreCase("the computer"))
+				playerVsAI(win);
+
+			else if(answer.equalsIgnoreCase("another player"))
+				playerVsPlayer(win);
+			else if(answer.equalsIgnoreCase("player"))
+				playerVsPlayer(win);
+
+			else
+				System.out.println("Invalid input.");
+
+			if(win == true)
+				System.out.println("Player 1 wins!");
+			else
+				System.out.println("Player 2 wins!");
+
+			System.out.println("Would you like to play again?");
+			String playAgain1 = sc.next();
+
+			if(playAgain1.equalsIgnoreCase("yes"))
+				playAgain2 = true;
+			else 
+				break;
+
+		} while (playAgain2 == true);
 	}
-
 }
+
+
