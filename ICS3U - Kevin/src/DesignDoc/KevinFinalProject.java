1117374ead09c6ef419423ff5dd6a6c8c0ea6aa1
348735036 <348735036@C-3ZZHYR1.tdsb.on.ca>
2020-01-14 13:54:33 -0500

    hi

--- ICS3U - Kevin/src/DesignDoc/KevinFinalProject.java
+++ ICS3U - Kevin/src/DesignDoc/KevinFinalProject.java
@@ -6,11 +6,20 @@ package DesignDoc;
  * Janurary 13, 2020
  */
 
+import java.util.Scanner;
+
 public class KevinFinalProject {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		
+		boolean playAgain;
+		
+		
+		//input
+		System.out.println("Would you like to play against the computer or another player?");
+		String answer = sc.next();
 		
 
 	}
