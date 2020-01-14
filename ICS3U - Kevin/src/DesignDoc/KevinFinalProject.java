0ac4919fafb1d0469a46dbac0842952d6a64e37d
348735036 <348735036@C-3ZZHYR1.tdsb.on.ca>
2020-01-14 13:54:25 -0500

    hi

--- ICS3U - Kevin/src/DesignDoc/KevinFinalProject.java
+++ ICS3U - Kevin/src/DesignDoc/KevinFinalProject.java
@@ -1,9 +1,17 @@
 package DesignDoc;
 
+/**KevinFinalProject.java
+ * KevinFinalProject.java is 
+ * @author Kevin Ho
+ * Janurary 13, 2020
+ */
+
 public class KevinFinalProject {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		
 
 	}
 
